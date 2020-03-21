14d7672a755737e6cc9494901dadb4082e758626
Arsonix1 <arsonix93@gmail.com>
2020-03-21 12:23:36 +1200

    Меню-аккордеон через JS

---
+++ script/main.js
@@ -0,0 +1,16 @@
+const menuItems = document.querySelectorAll('.menu-accordeon__title')
+menuItems.forEach(item => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    const menuItem = item.parentElement
+    if (menuItem.classList.contains('active')) {
+      menuItem.classList.remove('active')
+    } else {
+      const openedMenuElement = document.querySelector('.active')
+      if (openedMenuElement && !menuItem.isEqualNode(openedMenuElement)) {
+        openedMenuElement.classList.remove('active') 
+      }
+      menuItem.classList.add('active')
+    }
+  })
+})
