e893fa728af4e6e71922959473132d049cc69fec
Arsonix1 <arsonix93@gmail.com>
2020-03-31 14:15:11 +1200

    Ещё плюшки в аккордеоны

--- scripts/menu-accordeon.js
+++ scripts/menu-accordeon.js
@@ -1,13 +1,13 @@
-const menuItems = document.querySelectorAll('.menu-accordeon__title')
-menuItems.forEach(item => {
+const menuItems = $('.wrapper').find('.menu-accordeon__title')
+$(menuItems).each((ndx, item) => {
   $(item).on('click', event => {
     event.preventDefault()
-    const menuItem = item.parentElement
+    const menuItem = $(item).parent()
     if ($(menuItem).hasClass('menu-active')) {
       $(menuItem).removeClass('menu-active')
     } else {
-      const openedMenuElement = document.querySelector('.menu-active')
-      if (openedMenuElement && !menuItem.isEqualNode(openedMenuElement)) {
+      const openedMenuElement = $(menuItem).siblings('.menu-active')
+      if (openedMenuElement && menuItem !== openedMenuElement) {
         $(openedMenuElement).removeClass('menu-active')
       }
       $(menuItem).addClass('menu-active')
--- scripts/team.js
+++ scripts/team.js
@@ -1,13 +1,13 @@
-const teamItems = document.querySelectorAll('.team-accordeon__name')
-teamItems.forEach(item => {
+const teamItems = $('.wrapper').find('.team-accordeon__name')
+$(teamItems).each((ndx, item) => {
   $(item).on('click', event => {
     event.preventDefault()
-    const teamItem = item.parentElement
+    const teamItem = $(item).parent()
     if ($(teamItem).hasClass('team-active')) {
       $(teamItem).removeClass('team-active')
     } else {
-      const openedTeamElement = document.querySelector('.team-active')
-      if (openedTeamElement && !teamItem.isEqualNode(openedTeamElement)) {
+      const openedTeamElement = $(teamItem).siblings('.team-active')
+      if (openedTeamElement && teamItem !== openedTeamElement) {
         $(openedTeamElement).removeClass('team-active')
       }
       $(teamItem).addClass('team-active')
